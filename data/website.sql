7e3be963349e5268e795069fa20a86793b4b2e86
Chad Miller <chad.miller.3333@gmail.com>
2020-09-27 00:20:27 -0400

    Added missing test website

--- data/website.sql
+++ data/website.sql
@@ -1,5 +1,7 @@
 insert into tb_website ( url, website_type )
-     values ( 'https://www.are.na/evan-collins-1522646491/global-village-coffeehouse', 1 );
+     values ( 'https://www.are.na/evan-collins-1522646491/global-village-coffeehouse', 1 ),
+            ( 'https://www.facebook.com/groups/288047698281354', 2 );
 
 insert into tb_aesthetic_website ( aesthetic, website )
-     values ( 1, 1 );
+     values ( 1, 1 ),
+            ( 1, 2 );
