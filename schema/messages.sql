6f642413acc4687d19c89a0a5cd87ce8b376d575
Chad Miller <chad.miller.3333@gmail.com>
2020-12-07 23:04:56 -0500

    Added password reset

--- schema/messages.sql
+++ schema/messages.sql
@@ -37,4 +37,41 @@ $_$
 </p>
 $_$,
     'd-2dc30f75099544479e86c1057e2b520a'
+), (
+    2,
+    'Reset Your CARI Password',
+$_$
+Hello ${username},
+
+You are receiving this email because a request to reset your password has been made. If you did not request this, please ignore this message. Otherwise, please navigate to the link below to reset your password:
+${resetPasswordUrl}
+
+This link is valid for one hour.
+
+Thank you,
+the Consumer Aesthetics Research Institute
+$_$,
+$_$
+<p>Hello ${username},</p>
+
+<p>
+    You are receiving this email because a request to reset your password has been made.
+    If you did not request this, please ignore this message.
+    Otherwise, please click the link below to reset your password:
+</p>
+<p>
+    <a href="${resetPasswordUrl}">${resetPasswordUrl}</a>
+</p>
+<p>This link is valid for one hour.</p>
+<p>
+    Thank you,
+    <br />
+    - the Consumer Aesthetics Research Institute
+</p>
+$_$,
+    'd-2e9ac445cdce460b83e5abf119fed2d7'
 );
+
+update tb_message_template
+   set body_plaintext = regexp_replace( body_plaintext, '(^[\n\r])|([\n\r]$)', '', 'g' ),
+       body_html      = regexp_replace( body_html,      '(^[\n\r])|([\n\r]$)', '', 'g' );
