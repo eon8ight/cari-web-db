faf4e9cd114de928970fef80f90cc06ebef023b0
Chad Miller <chad.miller.3333@gmail.com>
2020-12-09 23:18:33 -0500

    Added system entity; added message template

--- schema/entity.sql
+++ schema/entity.sql
@@ -3,8 +3,30 @@ create sequence sq_pk_entity;
 create table tb_entity (
     entity        integer   primary key default nextval( 'sq_pk_entity'::regclass ),
     email_address text      not null unique,
-    username      text      not null unique,
-    password_hash text      not null,
-    created       timestamp not null default now(),
+    username      text      unique,
+    password_hash text,
+    inviter       integer   not null references tb_entity,
+    invited       timestamp not null default now(),
+    registered    timestamp,
     confirmed     timestamp
+);
+
+insert into tb_entity (
+    entity,
+    email_address,
+    username,
+    password_hash,
+    inviter,
+    invited,
+    registered,
+    confirmed
+) values (
+    0,
+    'no-reply@c-a-r-i.org',
+    'System Entity',
+    '',
+    0,
+    '1970-01-01 00:00:00',
+    '1970-01-01 00:00:00',
+    '1970-01-01 00:00:00'
 );
--- schema/messages.sql
+++ schema/messages.sql
@@ -1,77 +1,18 @@
 create table tb_message_template (
     message_template integer primary key not null,
-    subject          text not null,
-    body_plaintext   text not null,
-    body_html        text not null,
     ext_id           text not null unique
 );
 
 insert into tb_message_template (
     message_template,
-    subject,
-    body_plaintext,
-    body_html,
     ext_id
 ) values (
     1,
-    'Confirm Your CARI account',
-$_$
-Hello ${username},
-
-Please navigate to the link below to confirm your CARI account.
-${confirmUrl}
-
-Thank you,
-the Consumer Aesthetics Research Institute
-$_$,
-$_$
-<p>Hello {{username}},</p>
-<p>Please click the link below to confirm your CARI account.</p>
-<p>
-    <a href="{{confirmUrl}}">{{confirmUrl}}</a>
-</p>
-<p>
-    Thank you,
-    <br />
-    - the Consumer Aesthetics Research Institute
-</p>
-$_$,
     'd-2dc30f75099544479e86c1057e2b520a'
 ), (
     2,
-    'Reset Your CARI Password',
-$_$
-Hello ${username},
-
-You are receiving this email because a request to reset your password has been made. If you did not request this, please ignore this message. Otherwise, please navigate to the link below to reset your password:
-${resetPasswordUrl}
-
-This link is valid for one hour.
-
-Thank you,
-the Consumer Aesthetics Research Institute
-$_$,
-$_$
-<p>Hello ${username},</p>
-
-<p>
-    You are receiving this email because a request to reset your password has been made.
-    If you did not request this, please ignore this message.
-    Otherwise, please click the link below to reset your password:
-</p>
-<p>
-    <a href="${resetPasswordUrl}">${resetPasswordUrl}</a>
-</p>
-<p>This link is valid for one hour.</p>
-<p>
-    Thank you,
-    <br />
-    - the Consumer Aesthetics Research Institute
-</p>
-$_$,
     'd-2e9ac445cdce460b83e5abf119fed2d7'
+), (
+    3,
+    'd-8edb5b755fd04d4092c366fe24452ad9'
 );
-
-update tb_message_template
-   set body_plaintext = regexp_replace( body_plaintext, '(^[\n\r])|([\n\r]$)', '', 'g' ),
-       body_html      = regexp_replace( body_html,      '(^[\n\r])|([\n\r]$)', '', 'g' );
