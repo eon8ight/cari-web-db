3829f4506209253e1cf72f35d0b6636883417962
Chad Miller <chad.miller.3333@gmail.com>
2020-12-11 01:06:09 -0500

    Added roles; added more fields to entities

--- schema/entity.sql
+++ schema/entity.sql
@@ -1,14 +1,20 @@
 create sequence sq_pk_entity;
 
 create table tb_entity (
-    entity        integer   primary key default nextval( 'sq_pk_entity'::regclass ),
-    email_address text      not null unique,
-    username      text      unique,
-    password_hash text,
-    inviter       integer   not null references tb_entity,
-    invited       timestamp not null default now(),
-    registered    timestamp,
-    confirmed     timestamp
+    entity             integer   primary key default nextval( 'sq_pk_entity'::regclass ),
+    email_address      text      not null unique,
+    username           text      unique,
+    password_hash      text,
+    inviter            integer   not null references tb_entity,
+    invited            timestamp not null default now(),
+    registered         timestamp,
+    confirmed          timestamp,
+    first_name         text,
+    last_name          text,
+    biography          text,
+    title              text,
+    profile_image_url  text,
+    favorite_aesthetic integer references tb_aesthetic
 );
 
 insert into tb_entity (
@@ -19,14 +25,36 @@ insert into tb_entity (
     inviter,
     invited,
     registered,
-    confirmed
+    confirmed,
+    first_name,
+    last_name
 ) values (
     0,
     'no-reply@c-a-r-i.org',
-    'System Entity',
+    'root',
     '',
     0,
     '1970-01-01 00:00:00',
     '1970-01-01 00:00:00',
-    '1970-01-01 00:00:00'
+    '1970-01-01 00:00:00',
+    'System',
+    'Entity'
+);
+
+create table tb_role (
+    role  integer primary key,
+    label varchar(50) not null unique
+);
+
+insert into tb_role ( role, label )
+     values ( 1, 'Admin' ),
+            ( 2, 'User' );
+
+create sequence sq_pk_entity_role;
+
+create table tb_entity_role (
+    entity_role integer primary key default nextval( 'sq_pk_entity_role' ),
+    entity      integer not null references tb_entity,
+    role        integer not null references tb_role,
+    unique ( entity, role )
 );
