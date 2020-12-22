afe07f1c67b0871e84a10251d822128a3dfc45b4
Chad Miller <chad.miller.3333@gmail.com>
2020-12-22 00:08:06 -0500

    Replaced start and end year text inputs with drop-down selectors with constants

--- functions/fn_get_approximate_end_year.sql
+++ functions/fn_get_approximate_end_year.sql
@@ -2,21 +2,12 @@ CREATE OR REPLACE FUNCTION fn_get_approximate_end_year(
     pk_aesthetic integer
 ) RETURNS INTEGER AS
  $_$
-    select case
-             when end_year ~ '^\d+$'          then end_year::integer
-             when lower(end_year) = 'present' then date_part('year', CURRENT_DATE)::integer
-             else regexp_replace((regexp_split_to_array(end_year, '\s+(?=\S*$)'))[2], 's$', '')::integer
-                  + (
-                      case lower((regexp_split_to_array(end_year, '\s+(?=\S*$)'))[1])
-                        when 'very early' then 1
-                        when 'early'      then 3
-                        when 'mid'        then 5
-                        when 'late'       then 7
-                        when 'very late'  then 9
-                      end
-                    )
-           end
-      from tb_aesthetic
-     where aesthetic = pk_aesthetic;
+       select e.year + es.weight
+         from tb_aesthetic a
+    left join tb_era e
+           on a.end_era = e.era
+    left join tb_era_specifier es
+           on e.era_specifier = es.era_specifier
+        where a.aesthetic = pk_aesthetic
  $_$
 LANGUAGE sql;
--- schema/aesthetic.sql
+++ schema/aesthetic.sql
@@ -1,3 +1,42 @@
+create table tb_era_specifier (
+    era_specifier integer primary key,
+    label         varchar(20) not null unique,
+    weight        integer not null unique
+);
+
+insert into tb_era_specifier ( era_specifier, label, weight )
+     values ( 1, 'Very Early', 1 ),
+            ( 2, 'Early', 3 ),
+            ( 3, 'Mid', 5 ),
+            ( 4, 'Late', 7 ),
+            ( 5, 'Very Late', 9 );
+
+create table tb_era (
+    era           integer primary key,
+    era_specifier integer not null references tb_era_specifier,
+    year          integer not null,
+    unique ( era_specifier, year )
+);
+
+insert into tb_era (
+    era,
+    era_specifier,
+    year
+)
+  select row_number() over(),
+         es.era_specifier,
+         y.year
+    from tb_era_specifier es,
+         ( values
+           ( 1970 ),
+           ( 1980 ),
+           ( 1990 ),
+           ( 2000 ),
+           ( 2010 ),
+           ( 2020 )
+         ) as y ( year )
+order by y.year, es.era_specifier;
+
 create sequence sq_pk_aesthetic;
 
 create table tb_aesthetic (
@@ -5,8 +44,8 @@ create table tb_aesthetic (
     name             text       not null unique,
     url_slug         text       not null unique,
     symbol           varchar(3) unique,
-    start_year       text,
-    end_year         text,
+    start_era        integer    references tb_era,
+    end_era          integer    references tb_era,
     description      text       not null,
     media_source_url text
 );
