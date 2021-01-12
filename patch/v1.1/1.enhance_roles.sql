f198e741a83bcac3558de08b2d1a0f1cbccef6ab
Chad Miller <chad.miller.3333@gmail.com>
2021-01-11 21:57:43 -0500

    Removed entity-specific data from patch

--- patch/v1.1/1.enhance_roles.sql
+++ patch/v1.1/1.enhance_roles.sql
@@ -17,37 +17,3 @@ insert into tb_role ( role, label, rank )
             ( 7, 'Community Organizer', 50 ),
             ( 8, 'Webmaster', 60 ),
             ( 9, 'Web Developer', 70 );
-
-insert into tb_entity_role (
-    entity,
-    role
-)
-select e.entity,
-       v.role
-  from (
-         values ( 'alex.edwards', 7 ),
-                ( 'alex.edwards', 5 ),
-                ( 'chan.miller', 9 ),
-                ( 'cindy.hernandez', 4 ),
-                ( 'evan.collins', 4 ),
-                ( 'froyo.tam', 3 ),
-                ( 'jack.grimes', 5 ),
-                ( 'max.krieger', 6 ),
-                ( 'zovi.mcentee', 8 )
-       ) as v ( entity_username, role )
-  join tb_entity e
-    on v.entity_username = e.username;
-
-update tb_entity e
-   set title = v.title
-  from (
-         values ( 'alex.edwards', null ),
-                ( 'chan.miller', null ),
-                ( 'cindy.hernandez', 'Design Historian' ),
-                ( 'evan.collins', 'Architecture' ),
-                ( 'froyo.tam', 'Transdisciplinary Design' ),
-                ( 'jack.grimes', 'Designer' ),
-                ( 'max.krieger', 'Interactive Media' ),
-                ( 'zovi.mcentee', 'Project Manager' )
-       ) as v ( entity_username, title )
- where v.entity_username = e.username;
